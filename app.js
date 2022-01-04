fa99de03d94eaed32c98111faee70803d9ad68d0
Faiz E Arsal <faizarsal055@gmail.com>
2022-01-04 21:14:01 +0500

    wot

---
+++ app.js
@@ -0,0 +1,17 @@
+// var name = prompt ('Enter your name');
+// var roll = prompt ('Enter your roll number');
+// var school = prompt ('Enter your section');
+// var math = prompt ('Enter your Math marks');
+// var physics = prompt ('Enter your physics  marks');
+// var urdu = prompt ('Enter your urdu marks');
+// var  javascript= prompt ('Enter your javascript  marks');
+// var chemistry = prompt ('Enter your chemistry marks');
+// var obtaine_marks = parseInt(math) + parseInt(physics) + parseInt(urdu) + parseInt(javascript)+ parseInt(chemistry);
+// var percentange= obtaine_marks *100 / 500
+
+
+// document.write("Your Name:"+ name + "<br>");
+// document.write( "Roll No:"+roll+ "<br>");
+// document.write(  "Obtain Marks:"+obtaine_marks+ "<br>");
+// document.write("percentange:"+percentange +"<br>")
+
